bb4f4f5bf5eebda96e68494116863e3f765886c8
danmaclean <maclean.daniel@gmail.com>
2018-11-09 13:57:24 +0000

    initial

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='FluorSeg',
+    version='0.0.0dev',
+    packages=['fluorseg',],
+    license='BSD',
+    long_description=open('README.txt').read(),
+)
