466f190309a1ffbd86182e6e24ba501c3610f214
danmaclean <maclean.daniel@gmail.com>
2019-02-01 13:56:44 +0000

    insert error handling in find blobs

--- fluorseg/methods.py
+++ fluorseg/methods.py
@@ -75,7 +75,7 @@ def extract_small_blob_count(dirpath, quantile = 0.99, min_size = 4):
     max_projs_channel_one = [ liffile.max_proj(z_stacks) for z_stacks in lif.channel_one_images ]
     result.blobs_channel_1 = [liffile.find_blobs(mp, quantile, min_size) for mp in max_projs_channel_one]
     result.blob_count_channel_1 = [liffile.count_blobs(bi) for bi in result.blobs_channel_1 ]
-
+    return result
 
 def as_csv(result):
 
