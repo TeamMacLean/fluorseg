76a24e8672db304640796d235207101da3bd170c
danmaclean <maclean.daniel@gmail.com>
2019-02-01 14:36:23 +0000

    insert error handling in find blobs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='FluorSeg',
-    version='0.0.17dev',
+    version='0.0.18dev',
     packages=['fluorseg', 'fluorseg.test'],
     author='Dan MacLean',
     author_email='dan.maclean@tsl.ac.uk',
